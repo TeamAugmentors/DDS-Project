fdbe37ee44076294f93870a7d02238afba7954fa
Ahmed Saad Tanim <55749602+AhmedSaadTanim@users.noreply.github.com>
2022-09-05 23:32:09 +0600

    done

--- Sites/Dhaka Site/dhaka_conn.sql
+++ Sites/Dhaka Site/dhaka_conn.sql
@@ -5,7 +5,7 @@ create database link server_site
  using '(DESCRIPTION =
        (ADDRESS_LIST =
          (ADDRESS = (PROTOCOL = TCP)
-		 (HOST = 192.168.0.119)
+		 (HOST = 192.168.0.119) //host ip
 		 (PORT = 1521))
        )
        (CONNECT_DATA =
